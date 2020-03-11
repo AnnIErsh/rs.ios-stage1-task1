67737d38545c446f8e2b7e3efe491f47ceab1255
Anna <annaersh1994@gmail.com>
2020-03-11 17:19:57 +0300

    feat: add 1st exercise

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -2,9 +2,30 @@
 
 @implementation T1Array
 
-// Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *happyArray = [NSMutableArray arrayWithArray:sadArray];
+    int i = 0;
+    
+    if (happyArray.count <= 2)
+    {
+        return sadArray;
+    }
+    while (i < happyArray.count - 1)
+    {
+        if (i == 0)
+            i++;
+        if ([happyArray[i - 1] intValue] + [happyArray[i + 1] intValue] < [happyArray[i] intValue])
+        {
+            [happyArray removeObjectAtIndex:i];
+            happyArray = [[self convertToHappy:[happyArray mutableCopy]] mutableCopy];
+            return [happyArray copy];
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return sadArray;
 }
 
 @end
