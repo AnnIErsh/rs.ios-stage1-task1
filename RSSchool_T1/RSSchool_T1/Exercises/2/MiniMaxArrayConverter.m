f02c07e5d9649f5bbff3bf5252d8d916d737f3db
Anna <annaersh1994@gmail.com>
2020-03-12 00:18:30 +0300

    feat: add 2nd exercise

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -2,9 +2,24 @@
 
 @implementation MiniMaxArrayConverter
 
-// Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSMutableArray *arr = [[array sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+    NSUInteger maxSum = 0;
+    NSUInteger minSum = 0;
+    NSMutableArray *arrForMax = [arr mutableCopy];
+    NSMutableArray *arrForMin = [arr mutableCopy];
+    [arrForMin removeObjectAtIndex:0];
+    [arrForMax removeLastObject];
+    
+    for (int i = 0; i < arr.count - 1; i++)
+    {
+        maxSum += [arrForMax[i] intValue];
+        minSum += [arrForMin[i] intValue];
+    }
+    
+    
+    return @[[NSNumber numberWithUnsignedLong:maxSum], [NSNumber numberWithUnsignedLong:minSum]];
 }
 
 @end
