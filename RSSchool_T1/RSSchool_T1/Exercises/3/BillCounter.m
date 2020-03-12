812b5b0c445026ffceb7a0d441400029c1ced629
Anna <annaersh1994@gmail.com>
2020-03-12 18:42:18 +0300

    feat: add 3rd exercise

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -2,9 +2,25 @@
 
 @implementation BillCounter
 
-// Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    NSInteger sumOfAnna = 0;
+    NSMutableArray *billOfAnna = [bill mutableCopy];
+    NSInteger sumOfBill = 0;
+    [billOfAnna removeObjectAtIndex:index];
+    
+    for (int i = 0; i < billOfAnna.count; i++)
+    {
+        sumOfBill += [billOfAnna[i] intValue];
+    }
+    sumOfAnna = sumOfBill / 2;
+    
+    if ([sum intValue] == sumOfAnna)
+    {
+        return @"Bon Appetit";
+    }
+    
+    return [@([sum intValue] - sumOfAnna) stringValue];
 }
 
 @end
