d23f3b926705568e16f5376fc87e748e6f8b1905
Anna <annaersh1994@gmail.com>
2020-03-14 23:49:34 +0300

    fix: change 4th task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
@@ -3,7 +3,11 @@
 @interface Parser : NSObject
 
 @property (nonatomic, strong) NSMutableArray *array;
+@property (nonatomic, strong) NSMutableString *changeString;
+
 - (NSArray <NSString*>*)parseString: (NSString*)string;
+- (NSString*)ifStr:(NSString*)ch;
+- (NSString*)ifString:(char)ch;
 
 @end
 
--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -17,49 +17,85 @@
     }
     return @"";
 }
+
+- (NSString*)ifStr:(NSString*)ch {
+    if ([ch  isEqual: @")"])
+    {
+        return @"(";
+    }
+    if ([ch  isEqual: @"]"])
+    {
+        return @"[";
+    }
+    if ([ch  isEqual: @">"])
+    {
+        return @"<";
+    }
+    return @"";
+}
+
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-
-    NSMutableString *str = [string mutableCopy];
-    NSRange rangeOpen = [str rangeOfString:@"("];
-    NSRange rangeOpenTwo = [str rangeOfString:@"["];
-    NSRange rangeOpenThree = [str rangeOfString:@"<"];
-    NSRange rangeClose;
+    
+    NSRange leftP = [string rangeOfString:@"(" options:NSBackwardsSearch];
+    NSRange leftB = [string rangeOfString:@"[" options:NSBackwardsSearch];
+    NSRange leftBr = [string rangeOfString:@"<" options:NSBackwardsSearch];
+    self.changeString = [string mutableCopy];
+    unsigned long start = 0;
+    unsigned long end = 0;
     char c;
-    unsigned long open = 0;
-    unsigned long close = 0;
-
-    for (int i = 0; i < str.length; i++)
-    {
-        if (i == rangeOpen.location || i == rangeOpenTwo.location || i == rangeOpenThree.location)
+    unsigned long i = string.length;
+    
+    for (;i ;i--) {
+        if (i == leftP.location || i == leftB.location || i == leftBr.location)
         {
-            open = i;
-            c = [[str copy] characterAtIndex:i];
-            NSString *newStr = [self ifString:c];
-            rangeClose = [[str copy] rangeOfString: newStr];
-            close = rangeClose.location - i;
-            break;
+            start = i;
+            c = [string characterAtIndex:start];
+            NSString *searchString = [string substringFromIndex:start];
+            NSString *bracket = [self ifString:c];
+            NSRange rightClose = [searchString rangeOfString: bracket];
+            NSRange starRange = [string rangeOfString:bracket];
+            
+            if ([self.changeString characterAtIndex: starRange.location] == '*')
+            {
+                searchString = [self.changeString substringFromIndex:start];
+                rightClose = [searchString rangeOfString:bracket];
+                searchString = [string substringFromIndex:start];
+            }
+            
+            end = rightClose.location;
+            NSString *substring = [searchString substringWithRange:NSMakeRange(1, end - 1)];
+            
+            if (self.array == nil) {
+                self.array = [NSMutableArray new];
+                [self.array addObject:substring];
+            }
+            else
+            {
+                [self.array addObject:substring];
+            }
+    
+            NSString *str = [self ifStr:bracket];
+            [self.changeString replaceCharactersInRange:NSMakeRange(i + substring.length + 1, 1) withString:@"*"];
+            [self.changeString replaceCharactersInRange:NSMakeRange(i, 1) withString:@"*"];
+            
+            if (i == leftP.location)
+            {
+                leftP = [self.changeString rangeOfString:str options:NSBackwardsSearch];
+            }
+            
+            if (i == leftB.location)
+            {
+                leftB = [self.changeString rangeOfString:str options:NSBackwardsSearch];
+            }
+            
+            if (i == leftBr.location)
+            {
+                leftBr = [self.changeString rangeOfString:str options:NSBackwardsSearch];
+            }
         }
     }
-
-    if (open && close)
-    {
-        NSRange range = NSMakeRange(open + 1, close - 1);
-        NSRange rangeBracket = NSMakeRange(open, close + 1);
-        NSString *subString = [[str copy] substringWithRange:range];
-        [str deleteCharactersInRange:rangeBracket];
-        
-        if (self.array == nil) {
-            self.array = [NSMutableArray new];
-            [self.array addObject:subString];
-        }
-        else
-        {
-            [self.array addObject:subString];
-        }
-        [self parseString:subString];
-        [self parseString:[str copy]];
-    }
-    return [self.array copy];
+    NSArray *results = [[self.array reverseObjectEnumerator] allObjects];
+    return results;
 } 
 
 @end
